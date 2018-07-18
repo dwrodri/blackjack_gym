be1652e4e92c0a2324af1b7c7241ae741799070d
Derek Rodriguez <derek.rodriguez@protonmail.ch>
2018-07-18 19:17:19 -0400

    implemented hit() and stand()

--- src/GameInstance.c
+++ src/GameInstance.c
@@ -2,14 +2,11 @@
 // Created by Derek Rodriguez on 5/13/18.
 //
 
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "GameInstance.h"
 
-void add_bet(player *p, unsigned int bet_qty)
+void add_bet(player_t *p, unsigned int bet_qty)
 {
-    POOL[CURR_AMT_OF_BETS].bet_source = p;
+    POOL[CURR_AMT_OF_BETS].bet_src = p;
     POOL[CURR_AMT_OF_BETS].qty = bet_qty;
     CURR_AMT_OF_BETS++;
 }
@@ -26,8 +23,7 @@ void clear_bets()
 
 void shuffle_shoe()
 {
-    for (unsigned int i = (AMT_OF_DECKS*52)-1; i > 0; i--)
-    {
+    for (unsigned int i = (AMT_OF_DECKS*52)-1; i > 0; i--) {
         // TODO: make this more random
         srand(time(NULL));
         // Pick a random index from 0 to i
@@ -40,16 +36,20 @@ void shuffle_shoe()
     }
 }
 
-void deal(player *p)
+void deal(player_t *p, unsigned short amt_dealt)
 {
-    p->hand[p->amt_of_cards] = SHOE[TOP_OF_SHOE++];
-    p->hand[p->amt_of_cards + 1] = SHOE[TOP_OF_SHOE++];
-    p->amt_of_cards += 2;
+    for(unsigned short i = 0; i < amt_dealt; i++) {
+        p->hand[p->amt_of_cards + i] = SHOE[TOP_OF_SHOE++];
+    }
+    p->amt_of_cards += amt_dealt; 
 }
 
-void get_move()
+char get_move()
 {
-
+    printf("What would you like to do?[h]it/[s]tand/[d]ouble: ");
+    char userin[2];
+    scanf("%s", userin);
+    return userin[0];
 }
 
 const short calc_hand(const unsigned short *hand, const unsigned short hand_len)
@@ -57,9 +57,8 @@ const short calc_hand(const unsigned short *hand, const unsigned short hand_len)
     short score = 0;
     for(unsigned short i = 0; i < hand_len; i++) {
 
-        switch (hand[i])
-        {
-        case 0: // use sign bit to track if ace is in player's hand
+        switch (hand[i]) {
+        case 0: // use sign bit to track if ace is in player_t's hand
             score *= -1;
             break;
 
@@ -84,38 +83,38 @@ const short calc_hand(const unsigned short *hand, const unsigned short hand_len)
     return score;
 }
 
-void hit(player *p, player *d)
+void hit(player_t *p)
+{
+    deal(p, 1);
+}
+
+void d_down(player_t *p)
 {
 
 }
 
-void d_down(player *p)
+void stand(player_t *p)
+{
+    p->stood = true;
+}
+
+void split(player_t *p)
 {
 
 }
 
-void stand(player *p)
+void setup_new_game(player_t *p, unsigned int init_funds, player_t *d)
 {
-
-}
-
-void split(player *p)
-{
-
-}
-
-void setup_new_game(player *p, unsigned int init_funds, player *d)
-{
-    // clean up players
+    // clean up player_ts
     p->amt_of_cards = 0;
     d->amt_of_cards = 0;
     char* prompt = "Welcome to BJGym. You will be starting this round with %i credits.";
     printf(prompt, init_funds);
+
     // generate shoe
     for (unsigned short i = 0; i < (AMT_OF_DECKS*52); ++i) {
         SHOE[i] = (unsigned short)(i%13);
     }
-
     shuffle_shoe();
 
     // get initial bet
@@ -126,9 +125,9 @@ void setup_new_game(player *p, unsigned int init_funds, player *d)
     p->money -= strt_bet_qty;
     add_bet(p, strt_bet_qty);
 
-    // give cards to player and dealer
-    deal(p);
-    deal(d);
+    // give cards to player_t and dealer
+    deal(p, 2);
+    deal(d, 2);
 
     // DEBUG: dump state
     debug_state(p, d);
@@ -140,15 +139,14 @@ const bool is_over()
     return 0;
 }
 
-void debug_state(player *p, player *d)
+void debug_state(player_t *p, player_t *d)
 {
     puts("==========GAME STATE==========");
     printf("Player %x:\n", (unsigned int)p);
     printf("\tHand:\t");
     for(unsigned short i = 0; i < p->amt_of_cards; i++) {
 
-        switch (p->hand[i])
-        {
+        switch (p->hand[i]) {
         case 0: // ace
             printf("A\t");
             break;
@@ -173,8 +171,7 @@ void debug_state(player *p, player *d)
     printf("\tHand:\t");
     for(unsigned short i = 0; i < d->amt_of_cards; i++) {
 
-        switch (d->hand[i])
-        {
+        switch (d->hand[i]) {
         case 0: // ace
             printf("A\t");
             break;
@@ -195,32 +192,23 @@ void debug_state(player *p, player *d)
     printf("\n");
     printf("---\n");
 
-    printf("Bet:\tplayer\t\tqty\n");
+    printf("Bet:\tplayer_t\t\tqty\n");
 
-    for(short i = 0; i < CURR_AMT_OF_BETS; i++)
-    {
-        printf("\t%x\t%i\n", (unsigned int)POOL[i].bet_source, POOL[i].qty);
+    for(short i = 0; i < CURR_AMT_OF_BETS; i++) {
+        printf("\t%x\t%i\n", (unsigned int)POOL[i].bet_src, POOL[i].qty);
     }
     printf("---\n");
 
 }
 
-char get_player_decision(player *ag)
-{
-    printf("What would you like to do?[h]it/[s]tand/[d]ouble: ");
-    char userin[2];
-    scanf("%s", userin);
-    return *userin;
-}
-
-void process_move(char code)
+void process_move(char code, player_t *p)
 {
     switch(code) {
     case 'h' :
-        puts("you hit!");
+        hit(p);
         break;
     case 's' :
-        puts("you stood!");
+        stand(p);
         break;
     case 'd' :
         puts("doubling down!");
@@ -233,9 +221,12 @@ void process_move(char code)
 
 void exec_game_loop()
 {
-    player agent, dealer; // instantiate players
+    player_t agent, dealer; // instantiate player_ts
     agent.stood = false;
     setup_new_game(&agent, 100, &dealer);
-    char move_code = get_player_decision(&agent);
-    process_move(move_code);
+    while(!agent.stood){
+        char move_code = get_move();
+        process_move(move_code, &agent);
+        debug_state(&agent, &dealer);
+    }
 }
--- src/GameInstance.h
+++ src/GameInstance.h
@@ -2,6 +2,9 @@
 // Created by Derek Rodriguez on 5/13/18.
 //
 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 #ifndef BLACKJACK_GYM_GAMEINSTANCE_H
@@ -21,34 +24,34 @@ typedef struct
     unsigned short amt_of_cards;// amt of cards
     unsigned int money; // amount of money in wallet
 	bool stood;
-}player; //!< the datatype for storing player state
+}player_t; //!< the datatype for storing player_t state
 
 typedef struct
 {
-    player* bet_src; //<! mem address of player
+    player_t* bet_src; //<! mem address of player_t
     unsigned int qty; 
-}bet; //!< Think of this as like a log entry for the bets being placed. The actual ledger is the array. 
+}bet; //!< used as a log entry in the bet ledger.
 
 
 static unsigned short SHOE[AMT_OF_DECKS * 52]; //!< many decks
 static unsigned int TOP_OF_SHOE = 0; //!< index of top of deck
 static bet POOL[MAX_BETS]; //!< all bets placed during match go in the pool
-static unsigned short CURR_AMT_OF_BETS = 0; 
+static unsigned short CURR_AMT_OF_BETS = 0; //!< tracks amount of usage in the pool
 
 
-void add_bet(player *p,  unsigned int bet_qty); //! add bet to list
+void add_bet(player_t *p,  unsigned int bet_qty); //! add bet to list
 void clear_bets(); //! wipe all bets from POOL
 void shuffle_shoe(); //! shuffle the shoe
-void deal(player *p); //! take cards from shoe at place them in the player's hand
+void deal(player_t *p, unsigned short amt_dealt); //! take cards from shoe at place them in the player_t's hand
 const short calc_hand(const unsigned short *hand, const unsigned short hand_len); //! get score of hand
-void get_move(); //! read user input to perform move
-void hit(player *p); //! get new card
-void d_down(player *p); //! double down
-void stand(player *p); //! stop handing out cards to this player
-void split(player *p); //! split hand
-void setup_new_game(player *p, unsigned int init_funds, player *d); //! routine for new game
+char get_move(); //! read user input to perform move
+void hit(player_t *p); //! get new card
+void d_down(player_t *p); //! double down
+void stand(player_t *p); //! stop handing out cards to this player_t
+void split(player_t *p); //! split hand
+void setup_new_game(player_t *p, unsigned int init_funds, player_t *d); //! routine for new game
 const bool is_over(); //! check for bust
-void print_game_UI(); //! print human_friendly game state from player's POV
-void debug_state(player *p, player *d); //! print current game state
+void print_game_UI(); //! print human_friendly game state from player_t's POV
+void debug_state(player_t *p, player_t *d); //! print current game state
+void process_move(char code, player_t *p); //! perform action for player_t based on code
 void exec_game_loop(); //!main game loop
-
